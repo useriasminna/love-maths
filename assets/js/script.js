4577df7d74c13db593a43eb0e3978827531eaafd
useriasminna <pal.iasmina@yahoo.com>
2022-02-08 15:00:46 +0000

    Add favicon, and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,11 @@ document.addEventListener("DOMContentLoaded", function(){
             }
         })
     }
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if(event.key ==="Enter"){
+            checkAnswer();
+        }
+    })
     runGame("addition");
 })
 
@@ -22,6 +27,8 @@ document.addEventListener("DOMContentLoaded", function(){
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     //Creates two random number between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
